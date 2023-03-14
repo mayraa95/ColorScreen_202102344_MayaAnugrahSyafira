bffa3addd49b4f4dea71f43b65235c60e9435e77
Endiai <di.pardomuan11@gmail.com>
2023-03-14 10:46:02 +0700

    first commit

---
+++ app/src/main/java/com/mayanugrahsyafira/projekpert2/MainActivity.java
@@ -0,0 +1,16 @@
+package com.mayanugrahsyafira.projekpert2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mayaanugrahsyafira_202102344.helloworld.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
